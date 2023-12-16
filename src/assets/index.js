9046ecd587ead8356270f2b1f15258771cf015e7
Yuvrajhere <yuvrajisbest13@gmail.com>
2023-12-17 04:13:01 +0530

    feat: update info

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import recro from "./company/recro.png";
+import flam from "./company/flam.png";
+import habbit from "./company/habbit.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  flam,
+  recro,
+  habbit,
   carrent,
   jobit,
   tripguide,
